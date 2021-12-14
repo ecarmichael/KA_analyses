724f7944d797c14ad731403e62ef3f1b6b0571a1
ecarmichael <ecarmichael@gmail.com>
2021-12-14 00:47:45 -0500

    implemented tones and during tone sampling period

--- trial_control_sandbox.m
+++ trial_control_sandbox.m
@@ -1,9 +1,8 @@
 %% setup
 
 % timing
-total_time = 1 ; % total time to run converted from minutes to seconds.
+total_time = .5 ; % total time to run converted from minutes to seconds.
 total_time = total_time * 60; % converted from minutes to seconds.
-dt = 0.05; % sampling rate. 0.05 is much faster than you probably need.
 
 % states
 states = repmat([1;2],100,1); % lazy way to repeating numbers.
@@ -12,15 +11,29 @@ prior_states = [];
 trials = [];
 iState = 0; % initial state;
 
+%tones
+Fs=44100;                       %CD quality - also conveniently divisible by 30 and 25
+stim_dur=5;               %duration of puretone in seconds
+T=0:1/Fs:stim_dur-1/Fs;
+
+%tone 1
+t1_freq =8000; 
+Y1 = sin(2*pi*t1_freq*T);
+%tone2
+t2_freq = 1000; 
+Y2 = sin(2*pi*t2_freq*T);
+
 % make a figure to track everything
 figure(101)
 xlim([.5 .75])
 ylim([.45 .55])
 
-for iT = 0:dt:total_time
-    eTime = sprintf('%2.1f', iT); % time in a nice string format.
+t0 = clock; 
+
+while etime(clock, t0) < total_time
+    this_time = sprintf('%2.1f', etime(clock, t0)); % time in a nice string format.
     
-    if iT > 30
+    if etime(clock, t0) > 10
         if iState == 0 % move to a state;
             iState = datasample(states, 1);
             while length(prior_states) >= 3 &&  sum(prior_states(end-2:end) == iState) ==3 % check if the same state has appeared three times in a row.
@@ -30,22 +43,38 @@ for iT = 0:dt:total_time
         
         %update plot
         clf
-        text(.5, .5, ['Time: ' eTime 's'], 'FontSize', 24)
+        text(.5, .5, ['Time: ' this_time 's'], 'FontSize', 24)
         text(.5, .25, ['State: ' num2str(iState)], 'FontSize', 24)
         drawnow
         
         if iState == 1
-            licks = 0; % initialize licks.
+            tone_t = clock; % get elapsed time. 
+            licks = []; % initialize licks.
             
-            %            play a tone;
+            % play a tone; log the time. 
+            sound(Y1, Fs)
+            
+           while etime(clock, tone_t) < 5
+                
+                % lick check function here. check for licks (during tone)% when writing this make sure it logs the time. time points can be counted later and can also be added to the plot.
+                clf
+                text(.5, .5, ['Time: ' this_time 's'], 'FontSize', 24)
+                text(.5, .25, ['State: Tone 1'], 'FontSize', 24, 'color', 'b')
+                drawnow
+            end
+     
             
             
-            %check for licks (during tone)% when writing this make sure it
-            %logs the number and the time.
+            %check for licks
             
             
-            % do a thing like fire valve;
+            % do a thing like fire valve; log the time. 
+            valve_t = clock; 
             
+            %check for post-valve licks.  Could also be done later but this
+            %way you can plot them in a different color or track
+            %correct/incorrect responses in real time. 
+            post_licks = []; 
             
             % log it and return to 0 state
             prior_states(end+1) = iState;
@@ -53,22 +82,33 @@ for iT = 0:dt:total_time
             trials{end+1} = [];
             trials{end}.state = iState;
             trials{end}.licks = licks;
+            trials{end}.tone_time = tone_t; 
+            trials{end}.valve_t = valve_t; 
             % add in other details.
-            
             
             iState = 0; % return to 0 state.
             
         elseif iState == 2
-            licks = 0; % initialize licks.
+            licks = []; % initialize licks.
             
-            %play a tone;
+            % play a tone; log the time. 
+            tone_t = clock; 
+            tic; sound(Y2, Fs); toc; % seems to take .33 seconds to play the tone. 
+
+            while etime(clock, tone_t) < 5
+                % lick check function here. check for licks (during tone)% when writing this make sure it logs the time. time points can be counted later and can also be added to the plot.
+                clf
+                text(.5, .5, ['Time: ' this_time 's'], 'FontSize', 24)
+                text(.5, .25, ['State: Tone 2'], 'FontSize', 24, 'color', 'r')
+                drawnow
+            end
             
             
-            %check for licks%
+            % do a thing like fire valve; log the time. 
+            valve_t = clock; 
             
-            
-            % do a thing like fire valve;
-            
+            %check for post-valve licks
+            post_licks = []; 
             
             % log it and return to 0 state
             prior_states(end+1) = iState;
@@ -76,6 +116,8 @@ for iT = 0:dt:total_time
             trials{end+1} = [];
             trials{end}.state = iState;
             trials{end}.licks = licks;
+            trials{end}.tone_time = tone_t; 
+            trials{end}.valve_t = valve_t; 
             % add in other details.
             
             
@@ -85,7 +127,7 @@ for iT = 0:dt:total_time
     else
         
         clf
-        text(.5, .5, ['Time: ' eTime 's'], 'FontSize', 24)
+        text(.5, .5, ['Time: ' this_time 's'], 'FontSize', 24)
         text(.5, .25, 'State: waiting...', 'FontSize', 24)
         drawnow
     end
@@ -95,5 +137,7 @@ end % main loop
 
 %% save things.
 
+% save useful variables with dates and times.  Probably good to add a
+% subect ID or save it in a specific directory. 
 save(['Trials_' datestr(date, 'yyyy_mm_dd') '_' datestr(now, 'HH_MM')], 'trials')
 
